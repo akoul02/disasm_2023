50ab43cf9fae96224a6d305d81809d668f46bc08
akoul02 <akoul02@yandex.ru>
2023-04-08 16:54:21 +0300

    disasm+basic_stat

--- instruction_stats.py
+++ instruction_stats.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 
 class InstructionStatistics:
     def __init__(self, filename):
@@ -10,16 +11,17 @@ class InstructionStatistics:
         with open(self.filename, 'r') as file:
             lines = file.readlines()[3:-4] # Ignore first 3 and last 4 lines
             for line in lines:
-                # Extract the ARM instruction as a single string that ends with whitespace
+                # Extract the ARM instruction as a single string
                 fields = line[19:].strip().split()
                 if fields:
-                    instruction = fields[0]
-                    if instruction.isalpha():
-                        # Check if the instruction has two variants based on the last argument
-                        if fields[-1].startswith("#"):
-                            instruction += "_i"
-                        self.instructions[instruction] = self.instructions.get(instruction, 0) + 1
-                        self.num_instructions += 1
+                    # Replace "#0x" with "#imm" in the instruction string
+                    instruction_str = " ".join(fields)
+                    instruction_str = re.sub(r'#0x([\da-fA-F]+)', r'#imm', instruction_str)
+                    # Add register arguments to the instruction string
+                    instruction = instruction_str.split()[0] + " " + " ".join(instruction_str.split()[1:])
+                    # Check if the instruction has two variants based on the last argument
+                    self.instructions[instruction] = self.instructions.get(instruction, 0) + 1
+                    self.num_instructions += 1
 
     def print_statistics(self):
         if self.num_instructions == 0:
@@ -27,9 +29,11 @@ class InstructionStatistics:
         else:
             total_instructions = sum(self.instructions.values())
             print("Instruction statistics:\n")
+            self.instructions = dict(sorted(self.instructions.items(), key=lambda x: x[1], reverse=True))
             for instruction, count in self.instructions.items():
                 percentage = count/total_instructions * 100
                 print(f"{instruction}: {count} ({percentage:.2f}%)")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Count the frequency of ARM instructions in a binary file")
